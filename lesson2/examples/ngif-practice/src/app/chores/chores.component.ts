6b862a3a97dbd8f45346f83d7f8b509f5ffd0bf0
Nathan-422 <natiouswright@gmail.com>
2022-12-11 17:38:47 -0600

    added ngIf statements to chores example

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = []//['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   finishedChores = ['Fuck around', 'Kick rocks', 'Beat up biker gang', 'Debauchery', 'Put greese down the drain', 'Put your dick in the box'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   target = 'https://cdn2.iconfinder.com/data/icons/sports-110/48/archery-1024.png'
 
    constructor() { }
 
