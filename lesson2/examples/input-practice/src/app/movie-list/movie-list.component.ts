5e553c7e14281f108025e9cc1b2da8e70e7c7002
Nathan <natiouswright@gmail.com>
2022-12-12 11:04:20 -0600

    Finished input-practice example

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
+   displayWarning = false;
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(input:string) {
+      if (input === "") {
+         this.displayWarning = true;
+         return;
+      } else if (this.movies.includes(input.trim())) {
+         this.displayWarning = true;
+         return;
+      }
+      
+      this.displayWarning = false;
+      this.movies.push(input);
    }
 }
