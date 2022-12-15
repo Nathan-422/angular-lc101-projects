6ce4a879e25b7a9f0d278a7e2d3ba457ca69840f
Nathan-422 <natiouswright@gmail.com>
2022-12-15 16:11:01 -0600

    added joke buttons

--- lesson3/examples/src/app/joke-buttons/joke-buttons.component.ts
+++ lesson3/examples/src/app/joke-buttons/joke-buttons.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-joke-buttons',
+  selector: 'joke-buttons',
   templateUrl: './joke-buttons.component.html',
   styleUrls: ['./joke-buttons.component.css']
 })
 export class JokeButtonsComponent implements OnInit {
+
+  buttonHeading = "Working Buttons"
+  clickTime = false;
+  position = "right";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  stayStill() {
+    let positions = ['left', 'center', 'right']
+
+    positions.splice(positions.indexOf(this.position), 1)
+
+    this.position = positions[Math.floor(Math.random() * positions.length)]
+  }
 }
