62e5c2aca9348aa73338af891e95af1dbcb5e327
Nathan-422 <natiouswright@gmail.com>
2022-12-15 15:29:20 -0600

    init joke-buttons

---
+++ lesson3/examples/src/app/joke-buttons/joke-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joke-buttons',
+  templateUrl: './joke-buttons.component.html',
+  styleUrls: ['./joke-buttons.component.css']
+})
+export class JokeButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
