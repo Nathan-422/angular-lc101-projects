c97a5df128bd63992d44a2c8a37868f3b02649ac
Nathan-422 <natiouswright@gmail.com>
2022-12-16 21:14:41 -0600

    Movement function should be about working

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,28 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff(): void {
+    let result = window.confirm("Are you sure the shuttle is ready for takeoff?")
+    if (result) {
+      this.color = "blue";
+      this.height = 10000;
+      this.width = 0;
+      this.message = "Shuttle in flight."
+    }
+  }
+
+  moveRocket(rocketImage: HTMLElement, direction: string):void {
+    let hPos: number = parseInt(rocketImage.style.left);
+    let vPos: number = parseInt(rocketImage.style.bottom);
+    
+    switch (direction) {
+      case 'right':
+        console.log("right")
+        rocketImage.style.left = hPos + 10 + 'px';
+        this.width += 10000;
+        break;
+      
+    }
+  }
 }
