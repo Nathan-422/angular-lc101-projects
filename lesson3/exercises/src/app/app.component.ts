a2c4f5fb684ef063178a5f939735c444cfcf1540
Nathan-422 <natiouswright@gmail.com>
2022-12-16 22:07:31 -0600

    buttons and logic working

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,39 +1,73 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
+  title = "Exercises: Angular Lesson 3";
 
-  color = 'green';
+  color = "green";
   height = 0;
   width = 0;
-  message = 'Space shuttle ready for takeoff!';
+  message = "Space shuttle ready for takeoff!";
+  grounded = true;
 
-  handleTakeOff(): void {
-    let result = window.confirm("Are you sure the shuttle is ready for takeoff?")
+  handleTakeOff(rocketImage: HTMLElement): void {
+    let result = window.confirm(
+      "Are you sure the shuttle is ready for takeoff?"
+    );
     if (result) {
+      this.grounded = false;
       this.color = "blue";
-      this.height = 10000;
+      // this.height = 10000;
+      this.moveRocket(rocketImage, "up")
       this.width = 0;
-      this.message = "Shuttle in flight."
+      this.message = "Shuttle in flight.";
     }
   }
 
-  moveRocket(rocketImage: HTMLElement, direction: string):void {
+  moveRocket(rocketImage: HTMLElement, direction: string): void {
     let hPos: number = parseInt(rocketImage.style.left);
     let vPos: number = parseInt(rocketImage.style.bottom);
-    
+
     switch (direction) {
-      case 'right':
-        console.log("right")
-        rocketImage.style.left = hPos + 10 + 'px';
+      case "right":
+        rocketImage.style.left = hPos + 10 + "px";
         this.width += 10000;
         break;
-      
+      case "left":
+        rocketImage.style.left = hPos - 10 + "px";
+        this.width -= 10000;
+        break;
+      case "up":
+        rocketImage.style.bottom = vPos + 10 + "px";
+        this.height += 10000;
+        break;
+      case "down":
+        rocketImage.style.bottom = vPos - 10 + "px";
+        this.height -= 10000;
+        break;
     }
+
+    
+  }
+
+  land(rocketImage: HTMLElement): void {
+    let land = window.confirm("Are you ready to land?")
+    if (land) {
+      rocketImage.style.bottom = '0px'
+      this.height = 0;
+      this.color = 'green'
+      this.grounded = true;
+    }
+  }
+  abort(rocketImage: HTMLElement): void {
+    window.alert("Abort sequence activated.")
+    rocketImage.style.bottom = '0px'
+    this.height = 0;
+    this.color = 'green'
+    this.grounded = true;
   }
 }
